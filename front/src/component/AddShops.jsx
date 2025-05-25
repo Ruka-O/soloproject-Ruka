bda72df6dfd24b34c63fa3933b6ad456ed8dc45f
ruka <ruka_ootani@mail.toyota.co.jp>
2025-05-25 12:28:11 +0900

    useRef used

--- front/src/component/AddShops.jsx
+++ front/src/component/AddShops.jsx
@@ -1,31 +1,37 @@
 import list from '../data/prefecture';
 import sns from '../data/sns';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 function AddShops(props) {
 	const prefecture = list.data;
 	const snsList = sns.data;
-	const [storeName, setStorename] = useState('');
-	const [storePrefecture, setStorePrefecture] = useState('');
-	const [selectSns, setSelectSns] = useState('Instagram');
-	const [inputComment, setInputComment] = useState('');
-	const [makeTag, setMakeTag] = useState('');
-	const [url, setUrl] = useState('');
+	let storeName = useRef(null);
+	// const [storeName, setStorename] = useState('');
+	let storePrefecture = useRef(null);
+	// const [storePrefecture, setStorePrefecture] = useState('');
+	let selectSns = useRef(null);
+	// const [selectSns, setSelectSns] = useState('Instagram');
+	let inputComment = useRef(null);
+	// const [inputComment, setInputComment] = useState('');
+	let makeTag = useRef(null);
+	// const [makeTag, setMakeTag] = useState('');
+	let url = useRef(null);
+	// const [url, setUrl] = useState('');
 	const [addPush, setAddPush] = useState(false);
 
-	const setValue = (func, e) => {
-		func(e.target.value);
-	};
+	// const setValue = (func, e) => {
+	// 	func(e.target.value);
+	// };
 
 	const sendDetail = async () => {
 		if (storeName !== '') {
-			const tags = makeTag.replaceAll(' ', ', ');
+			const tags = makeTag.current.value.replaceAll(' ', ', ');
 			const registration = {
-				store_name: storeName,
-				prefecture: storePrefecture,
-				sns_name: selectSns,
-				url: url,
-				comment: inputComment,
+				store_name: storeName.current.value,
+				prefecture: storePrefecture.current.value,
+				sns_name: selectSns.current.value,
+				url: url.current.value,
+				comment: inputComment.current.value,
 				tags: tags,
 			};
 			await fetch('/api', {
@@ -52,12 +58,12 @@ function AddShops(props) {
 				<div id="send__detail">
 					<label>
 						店名：
-						<input type="text" onChange={(e) => setValue(setStorename, e)} />
+						<input type="text" ref={storeName} />
 					</label>
 					<br />
 
 					<label>都道府県：</label>
-					<select className="select" onChange={(e) => setValue(setStorePrefecture, e)}>
+					<select className="select" ref={storePrefecture}>
 						{prefecture.map((prefecture) => {
 							return (
 								<option value={prefecture} key={prefecture}>
@@ -69,7 +75,7 @@ function AddShops(props) {
 					<br />
 
 					<label>SNS：</label>
-					<select className="select" onChange={(e) => setValue(setSelectSns, e)}>
+					<select className="select" ref={selectSns}>
 						{snsList.map((sns) => {
 							return (
 								<option key={sns} value={sns}>
@@ -82,19 +88,19 @@ function AddShops(props) {
 
 					<label>
 						URL：
-						<input type="text" onChange={(e) => setValue(setUrl, e)} />
+						<input type="text" ref={url} />
 					</label>
 					<br />
 
 					<label id="comment">
 						コメント：
-						<input id="comment" type="text" onChange={(e) => setValue(setInputComment, e)} />
+						<input id="comment" type="text" ref={inputComment} />
 					</label>
 					<br />
 
 					<label>
 						タグ：
-						<input type="text" placeholder="単語をスペースで区切る" onChange={(e) => setValue(setMakeTag, e)} />
+						<input type="text" placeholder="単語をスペースで区切る" ref={makeTag} />
 					</label>
 					<br />
 
